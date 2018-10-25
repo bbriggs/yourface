7aafcaf4969b5f70baf1287038ad3d4464b6c64d
Bren Briggs <code@fraq.io>
2018-10-25 00:48:40 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yourface',
+
+    version='0.1.1',
+
+    license='MIT',
+
+    py_modules=['yourface'],
+
+    description="import your face"
+
+    author="Bren 'fraq' Briggs",
+
+    url="https://github.com/bbriggs/yourface",
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python'
+    ],
+
+)
